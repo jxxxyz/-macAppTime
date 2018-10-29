b59049fe4af711f946b10a245356cf0aab26bc48
macintosh <macintosh@macintoshdeMacBook-Pro.local>
2018-10-29 10:46:03 +0800

    Initial Commit

---
+++ EggTimer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EggTimer
+//
+//  Created by macintosh on 2018/10/29.
+//  Copyright © 2018 macintosh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
